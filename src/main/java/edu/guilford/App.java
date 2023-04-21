826a5c8285a62fc7f89d941a58939cea088137bf
LMehta0210 <lmehta@guilford.edu>
2023-04-21 10:18:14 -0400

    added user prompt for input

--- src/main/java/edu/guilford/App.java
+++ src/main/java/edu/guilford/App.java
@@ -1,5 +1,7 @@
 package edu.guilford;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -9,8 +11,12 @@ public class App
     public static void main( String[] args )
     {   
         //Intantiates two users
-        User user1 = new User("John", "Smith", "smithj@guilford.edu", "smithj", 19991231, "blue", "Fluffy", 6587, "pizza");
-        User user2 = new User("Jane", "Doe", "doej@guilford.edu", "doej", 19960229, "red", "Spot", 1928, "Ice Cream");
+        //User user1 = new User("John", "Smith", "smithj@guilford.edu", "smithj", 19991231, "blue", "Fluffy", 6587, "pizza");
+        //User user2 = new User("Jane", "Doe", "doej@guilford.edu", "doej", 19960229, "red", "Spot", 1928, "Ice Cream");
+
+        //creates two users
+        User user1 = createUser();
+        User user2 = createUser();
 
         //generates passwords for both users
         user1.generatePassword();
@@ -26,4 +32,40 @@ public class App
                 "\n\tKey: " + user2.getKey() +
                 "\n\tDecrypted password is: " + AES.decrypt(user2.encryptPassword(), user2.getKey()));
     }
+
+    //creates a method that prompts user for attributes and creates a user
+    public static User createUser() {
+        //creates scanner
+        Scanner input = new Scanner(System.in);
+
+        //prompts user for attributes
+        System.out.println("Enter first name: ");
+        String firstName = input.nextLine();
+        System.out.println("Enter last name: ");
+        String lastName = input.nextLine();
+        System.out.println("Enter email: ");
+        String email = input.nextLine();
+        System.out.println("Enter username: ");
+        String username = input.nextLine();
+        System.out.println("Enter date of birth (YYYYMMDD): ");
+        int dateOfBirth = input.nextInt();
+        System.out.println("Enter favorite color: ");
+        String favoriteColor = input.nextLine();
+        System.out.println("Enter pet name: ");
+        String petName = input.nextLine();
+        System.out.println("Enter favorite number: ");
+        int favoriteNumber = input.nextInt();
+        System.out.println("Enter favorite food: ");
+        String favoriteFood = input.nextLine();
+
+        //closes scanner
+        input.close();
+
+        //creates user
+        User user = new User(firstName, lastName, email, username, dateOfBirth, favoriteColor, petName, favoriteNumber, favoriteFood);
+
+        //returns user
+        return user;
+    }
+
 }
